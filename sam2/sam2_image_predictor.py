d81d9e9c0ba02b377c910d1e44aa596a6a675a3e
williampettit <14142910+williampettit@users.noreply.github.com>
2025-04-30 16:33:41 -0400

    disable mask thresholding during aggregation in image predictor

--- sam2/sam2_image_predictor.py
+++ sam2/sam2_image_predictor.py
@@ -369,7 +369,7 @@ class SAM2ImagePredictor:
                 mask_list.append(deaug_mask)
 
         # Use TTAManager for aggregation and thresholding
-        return self._tta_manager.aggregate_masks(mask_list)
+        return self._tta_manager.aggregate_masks(mask_list, apply_threshold=False)
 
     def _prep_prompts(
         self, point_coords, point_labels, box, mask_logits, normalize_coords, img_idx=-1
